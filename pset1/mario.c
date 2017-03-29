20a61af0fc919e63488ee74eb2dae05c055f5d49
shpp-isahnovskii <yaivan13@gmail.com>
2017-03-30 00:18:22 +0300

    first commit

--- pset1/mario.c
+++ pset1/mario.c
@@ -16,64 +16,39 @@ int get_height()
     while (height < 0 || height > PYRAMID_MAX_HEIGHT);
     return (height);
 }
-
-// void bloks_and_spaces(int pyramid_height, int n_row)   /*first version*/
-// {
-//     int pyramid_spaces = pyramid_height - N_BLOKS_AT_TOP;
-//     int pyramid_bloks = pyramid_height - pyramid_spaces;
-    
-//     for(int h = 0; h < n_row +1; h++)
-//     {
-//         for (int i = 0; i < pyramid_spaces; i++)
-//         {
-//             printf("-");
-//         }
-//         pyramid_spaces--;
-        
-//         for(int n = 0; n < pyramid_bloks; n++)
-//         {
-//             printf("#");
-//         }
-//         pyramid_bloks++;
-//         printf("\n");
-//     }
-//}
 //====================================================================== patern
-void row_builder(int pyramid_spaces, int pyramid_bloks)
+void row_builder(int static_pyramide_height, int local_pyramide_height)
+
 {
-    for (int i = 0; i < pyramid_spaces - 1 + N_BLOKS_AT_TOP; i++)
+    for (int i = 0; i < local_pyramide_height + 1 - N_BLOKS_AT_TOP; i++)
     {
-        printf(" ");
+        printf("-");
     }
-    for(int n = 0; n < pyramid_bloks + pyramid_bloks; n++) //more comfortable, add "..+ pyramid_bloks;"
+    for(int n = 0; n < static_pyramide_height * 2 - local_pyramide_height * 2 + N_BLOKS_AT_TOP * 2; n++) //more comfortable, add "..* 2;"
     {
         printf("#");
-        if(n == pyramid_bloks - 1)  //more comfortable, add "if()"
+        if(n == static_pyramide_height - local_pyramide_height + 1)  //more comfortable, add "if()"
         {
-            printf("  ");
+            printf("^^");
         }
     }
 
     printf("\n");
 }
-//====================================================================== builder
-void calculation(int X) //row_builder inside
+//======================================================================
+void printPyramide(int static_pyramide_height) //
 {
+    int local_pyramide_height = static_pyramide_height;
     
-}
-//====================================================================== green flag
-int main(void)
-{
-    int pyramid_height = get_height();
-    //bloks_and_spaces(pyramid_height, pyramid_height - N_BLOKS_AT_TOP); /*first version*/
-    
-    int pyramid_bloks = N_BLOKS_AT_TOP;
-    int pyramid_spaces = pyramid_height - pyramid_bloks;
-    
-    for(int h = 0; h < pyramid_height; h++)
+    for(int h = 0; h < static_pyramide_height; h++)
     {
-        row_builder(pyramid_spaces, pyramid_bloks);
-        pyramid_bloks++;
-        pyramid_spaces--;
-    }
+        row_builder(static_pyramide_height, local_pyramide_height);
+        local_pyramide_height--;
+    }  
+}
+//====================================================================== 
+int main()
+{
+    printPyramide(get_height());
+    return 0;
 }
